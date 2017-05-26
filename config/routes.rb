c09e071e9c064f95348a605635688bb3e245b5ae
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-05-26 10:03:57 +0200

    Afegir rutes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get 'statics/about'
 
 resources :peliculas
-
+root :to => "statics#index"
+get '/about' => 'static#about', :as => :about
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
