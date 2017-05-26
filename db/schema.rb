b69948106fba26a32bae82173bb418675807703e
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-05-26 13:09:24 +0200

    Genre Language Country

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526102949) do
+ActiveRecord::Schema.define(version: 20170526110744) do
 
   create_table "comentarios", force: :cascade do |t|
     t.text     "coment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
