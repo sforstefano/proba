44e50c97256b9d2fa808c81dabaeda50240772f0
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-05-26 14:08:26 +0200

    About

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'statics/index'
+  #get 'statics/index'
 
-  get 'statics/about'
+  #get 'statics/about'
 
 resources :peliculas
+
+get '/about' => 'statics#about', :as => :about
+
 root :to => "statics#index"
-get '/about' => 'static#about', :as => :about
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
