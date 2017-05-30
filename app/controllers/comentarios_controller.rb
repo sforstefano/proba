70e0de4960991a77654d997f51af9a28b4580eb2
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-05-30 09:53:43 +0200

    Añadir controlador comentarios

--- app/controllers/comentarios_controller.rb
+++ app/controllers/comentarios_controller.rb
@@ -1,2 +1,24 @@
 class ComentariosController < ApplicationController
+	def index
+		@comentarios = Comementario.all
+	end
+
+	def new
+		@comentario = Comentario.new
+	end
+
+	def create
+		@comentario = Comentario.new(comParams)
+		if @comentario.save
+			flash[:success] = "Comentario añadido."
+			redirect_to pelicula_path(@comentarios)
+		else
+			render 'new'
+		end
+	end
+
+	private
+		def comParams
+			params.require(:comentario).pemrit(:coment)
+		end
 end
