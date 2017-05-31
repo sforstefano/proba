e8daf0972b56fc7b75f87895a6178e7c6b85ef2f
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-05-31 19:23:32 +0200

    Modificar header i controlador per usuaris

--- app/controllers/peliculas_controller.rb
+++ app/controllers/peliculas_controller.rb
@@ -1,5 +1,5 @@
 class PeliculasController < ApplicationController
-
+before_action :authenticate_user!, except: [:index, :show]
    def index
       @peliculas = Pelicula.all
       #muestra las peliculas que hay en la base de datos
