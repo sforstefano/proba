419fb56f5d9eec610d200b2099acc3c3935b9d74
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-06-04 23:17:02 +0200

    Afegir gema AdminUser

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'coffee-script-source', '1.8.0'
 
 gem 'paperclip', '~> 5.1'
 
+gem 'activeadmin', '~> 1.0'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 root :to => "peliculas#index"
 get '/about' => 'statics#about', :as => 'about'
   devise_for :users
