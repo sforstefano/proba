b7b92893d1e823c9a87454cf30ecdab0b2d54a61
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-06-12 10:17:08 +0200

    Tornar a pas avans del desastre

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608152736) do
+ActiveRecord::Schema.define(version: 20170612081015) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 20170608152736) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "comentarios", force: :cascade do |t|
+    t.text     "coment"
+    t.integer  "pelicula_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["pelicula_id"], name: "index_comentarios_on_pelicula_id"
   end
 
   create_table "countries", force: :cascade do |t|
